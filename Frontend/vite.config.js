4f6692f29d2ec85fd7bca65f4d2f7dabaf521ece
Yash <yash20050523@gmail.com>
2025-10-15 15:33:18 +0530

    Update changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "copy-redirects",
+      closeBundle() {
+        fs.copyFileSync("public/_redirects", "dist/_redirects");
+      },
+    },
+  ],
+});
